7222a04e9570d2f3a40b301b2d95ca4de8147ca5
Ammar Al Hasani <ammarsalem2011@gmail.com>
2023-08-20 13:56:27 +0400

    Add files via upload

---
+++ dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void main(){
+  int x=5;
+  double y=3.55;
+  String name="Monday";
+  print(name.split(''));
+  print("Monday $x of Augest");
+  print(x+y);
+  print(name[0] );
+  print(name[1] );
+  print(name[2] );
+  print(name[3] );
+  print(name[4] );
+  print(name[5] );
+  print(name[0]+" "+name[1]+" "+name[2]+" "+name[3]+" "+name[4]+" "+name[5]);
+  var v=5.4;
+  var n=5;
+  var m=true;
+  var k='yputube';
+  n=6;
+  dynamic p="nome";
+  p=5;
+  DateTime d=DateTime.now();
+  print(d.minute.toString()+" "+d.second.toString()+" "+d.year.toString());
+ 
+}
